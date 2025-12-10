1ff19253d0cc567167372f2c1fff49bed62f7c9a
datchina03 <66707696+datchina03@users.noreply.github.com>
2025-12-10 12:35:16 +0530

    postgresql db migration and jwt authentication

--- src/main/java/com/example/cams/mf/config/SecurityConfig.java
+++ src/main/java/com/example/cams/mf/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,9 +18,11 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf(csrf->csrf.disable())
-		.authorizeHttpRequests(auth -> /*auth.requestMatchers("/register","/auth/login","/swagger-ui.html").permitAll()*/
-				auth.anyRequest().permitAll());
-		//httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		.authorizeHttpRequests(auth -> auth.requestMatchers("/register","/auth","/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html").permitAll().
+				anyRequest().authenticated());
+		httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return httpSecurity.build();
 	}
 	
